c4db0908aaf0d177ab46ceed4051e065c07afcb1
Eben Blaisdell <emblaisdell@foresthills.net>
2021-11-16 18:17:53 -0500

    log inputs and outputs

--- src/run.py
+++ src/run.py
@@ -9,12 +9,15 @@ def main():
 
     inputBytes = sys.stdin.buffer.read()
     inputs = [modulus.bytesToObject(*inp) for inp in zip(modulus.decode(inputBytes), func.inputTypes)]
+    print("inputs")
+    print(inputs)
 
     sys.stdout = open("./log", "w")
     outputs = func(inputs)
     sys.stdout = sys.__stdout__
 
     outputBytes = modulus.encode([modulus.objectToBytes(outp) for outp in outputs])
+    print("outputs")
     print(outputs)
     #sys.stdout.buffer.write(outputBytes)
     # XXX: is this necessary?
